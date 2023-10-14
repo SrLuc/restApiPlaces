bcea58c1db971e44149b7c37543570af3b89719c
unknown <ruanlucenaramos@gmail.com>
2023-10-14 13:26:31 -0300

    ar

--- README.md
+++
@@ -1,5 +0,0 @@
-# restApiPlaces
-
-When this api has completed, change the folder to nodejs repository.
-
-
--- middlewares/unsupported-routes.js
+++
@@ -1,7 +0,0 @@
-const HttpError = require("../models/error-model");
-
-const unsupportedRoutes = (req, res, next) => {
-  return next(new HttpError("Could not find this route.", 404));
-};
-
-module.exports = unsupportedRoutes;
